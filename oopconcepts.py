80d14920109b1ce81d31f7514beb114a90e2e4bc
Slava Petrov <slavik--petrov@yandex.ru>
2017-01-05 13:41:26 +0400

    Instantiation, __init__ and parameters

--- oopconcepts.py
+++ oopconcepts.py
@@ -1,7 +1,13 @@
 class Person:
 
-    def say_hello(self):
-        print("Hello!")
+    def __init__(self, name):
+        self.name = name
 
-p1 = Person()
+    def say_hello(self):
+        print("Hello, ", self.name)
+
+p1 = Person("Allan")
 p1.say_hello()
+
+p2 = Person("John")
+p2.say_hello()
