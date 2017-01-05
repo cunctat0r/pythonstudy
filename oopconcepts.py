315aedbfff9e345b1e4a4ffab999741bb9a802da
Slava Petrov <slavik--petrov@yandex.ru>
2017-01-05 14:00:35 +0400

    Create classroom and some encapsulation

--- oopconcepts.py
+++ oopconcepts.py
@@ -1,3 +1,18 @@
+class Classroom:
+
+    def __init__(self):
+        self._people = []
+
+    def add_person(self, person):
+        self._people.append(person)
+
+    def remove_person(self, person):
+        self._people.remove(person)
+
+    def greet(self):
+        for person in self._people:
+            person.say_hello()
+
 class Person:
 
     def __init__(self, name):
@@ -6,8 +21,8 @@ class Person:
     def say_hello(self):
         print("Hello, ", self.name)
 
-p1 = Person("Allan")
-p1.say_hello()
-
-p2 = Person("John")
-p2.say_hello()
+room = Classroom()
+room.add_person(Person("Scott"))
+room.add_person(Person("John"))
+room.add_person(Person("Paul"))
+room.greet()
