2ecf51ef7217cea2cb911a3aa47cc2754a898876
Slava Petrov <slavik--petrov@yandex.ru>
2017-01-27 11:47:01 +0400

    Add docstrings using Google Python Style convention

--- pyfund.py
+++ pyfund.py
@@ -1,7 +1,20 @@
+""" Retrieve and print words from a URL
+
+	Usage: python pyfund.py <url>
+"""
 import sys
 from urllib.request import urlopen
 
 def fetch_words(url):
+	""" Fetch a list of words from URL 
+
+	Args:
+		url: URL in UTF format
+
+	Returns:
+		A list of srtings 
+
+	"""
 	with urlopen(url) as story:
 		story_words = []
 		for line in story:
@@ -11,11 +24,21 @@ def fetch_words(url):
 	return story_words
 
 def print_items(items):
+	""" Print items of an iterable one per line
+
+	Args: 
+		items: iterable object
+	"""
 	for item in items:
 		print(item)
 
 
 def main(url):	
+	""" Print each word from a URL 
+
+	Args:
+		url: the URL of a text document
+	"""
 	words = fetch_words(url)
 	print_items(words)
 
